b2d77ff6829b037bf4827138dd200fb5b09b6c27
tanyuanji <tanyuanji@yihexinda.com>
2019-12-09 16:10:57 +0800

    change group id

--- src/main/java/cn/szsctc/App.java
+++ src/main/java/cn/szsctc/App.java
@@ -1,13 +1,37 @@
 package cn.szsctc;
 
+import cn.hutool.core.util.CharsetUtil;
+import cn.hutool.crypto.SecureUtil;
+import cn.hutool.crypto.symmetric.SymmetricAlgorithm;
+import cn.hutool.crypto.symmetric.SymmetricCrypto;
+
 /**
- * Hello world!
+ * Aes加密方式
  *
  */
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        String content = "test中文";
+
+        //随机生成密钥
+        byte[] key = SecureUtil.generateKey(SymmetricAlgorithm.AES.getValue()).getEncoded();
+
+        //构建
+        SymmetricCrypto aes = new SymmetricCrypto(SymmetricAlgorithm.AES, key);
+
+        //加密
+        byte[] encrypt = aes.encrypt(content);
+        //解密
+        byte[] decrypt = aes.decrypt(encrypt);
+
+        //加密为16进制表示
+        String encryptHex = aes.encryptHex(content);
+        //解密为字符串
+        String decryptStr = aes.decryptStr(encryptHex, CharsetUtil.CHARSET_UTF_8);
+
+
+        System.out.println( decryptStr );
     }
 }
